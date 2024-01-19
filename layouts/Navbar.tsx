fa12ccfba458954b4024546be3a15a736af5f3fc
Mohsin <mohsinshah3171@gmail.com>
2024-01-20 02:14:35 +0500

    FIX: Branding

--- layouts/Navbar.tsx
+++ layouts/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 
   return (
     <header className='bg-gradient-to-r from-cyan-500 to-slate-300 h-12 flex justify-between items-center px-4'>
-      <p className='text-xl'>Country Exchange App</p>
+      <p className='text-xl'>Currency Exchange App</p>
       <p className='text-red-600 cursor-pointer' onClick={handleLogout}>Logout</p>
     </header>
   )
